35b06734914fb558185c110470ce5cc7778d1afd
Mykhailo <rosmisha@gmail.com>
2024-10-14 21:49:29 +0300

    fix ddos attack my self in one episode

--- Infrastructure/DTO/Models/Internal/EventItemModelInternal.cs
+++ Infrastructure/DTO/Models/Internal/EventItemModelInternal.cs
@@ -3,6 +3,8 @@ namespace HowClient.Infrastructure.DTO.Models.Internal;
 public class EventItemModelInternal
 {
     public TimeSpan Duration { get; set; }
+    public bool ButtonLikeDisable { get; set; }
+    public bool ButtonDislikeDisable { get; set; }
     public bool AccessToggle { get; set; }
     public bool StatusToggle { get; set; }
 }
